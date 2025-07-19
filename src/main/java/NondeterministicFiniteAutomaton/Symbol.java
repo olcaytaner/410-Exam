319805ad430a02fa69c30b360fb4196d698c8213
Eren Yemşen <o.erenyemsen@gmail.com>
2025-07-19 20:48:58 +0300

    [NFA] fixes #2 and some small additions

--- src/main/java/NondeterministicFiniteAutomaton/Symbol.java
+++ src/main/java/NondeterministicFiniteAutomaton/Symbol.java
@@ -1,5 +1,9 @@
 package NondeterministicFiniteAutomaton;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Symbol {
 
     private char c;
@@ -12,10 +16,48 @@ public class Symbol {
         return c;
     }
 
+    public List<Warning> validate(){
+        List<Warning> warnings = new ArrayList<>();
+
+        if (!(isEpsilon() || Character.isLetter(getC()))){
+            warnings.add(new Warning("Symbol is not valid: " + getC(),  Warning.ERROR, -1, -1));
+        }
+        return warnings;
+    }
+
+    public boolean isEpsilon() {
+        return c == '_';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Symbol symbol = (Symbol) o;
+        return c == symbol.c;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(c);
+    }
+
     @Override
     public String toString() {
         return "Symbol{" +
                 "c=" + c +
                 "}\n";
     }
+
+    public String prettyPrint() {
+        StringBuilder sb = new StringBuilder();
+        String s;
+        if (isEpsilon()){
+            s = "epsilon";
+        }else {
+            s = String.valueOf(c);
+        }
+        sb.append("Symbol: ").append(s).append("\n");
+        return sb.toString();
+    }
+
 }
--- src/test/java/NondeterministicFiniteAutomaton/Main.java
+++ src/test/java/NondeterministicFiniteAutomaton/Main.java
@@ -5,11 +5,37 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        List<Warning> warnings = NFA.validate("src/test/java/NondeterministicFiniteAutomaton/NFAtest.txt");
-        System.out.println("warning count: " + warnings.size());
-        for (int i = 0; i < warnings.size(); i++) {
-            System.out.println(warnings.get(i));
+
+        String correctFilePath = "src/test/java/NondeterministicFiniteAutomaton/TestFiles/NFAtest-Correct.txt";
+
+        //Gets 10 Errors and 1 Warning and 2 correct transitions
+        String incorrectFilePath = "src/test/java/NondeterministicFiniteAutomaton/TestFiles/NFAtest-Incorrect.txt";
+
+        NFAParser nfaParser = new NFAParser(
+                correctFilePath
+        );
+
+        ParseResult parseResult = nfaParser.parseNFA();
+
+        NFA nfa = null;
+        if (parseResult.getErrorCount() == 0){
+            nfa = parseResult.getNfa();
+            System.out.println(nfa.prettyPrint());
         }
+
+        System.out.println("Syntax warning(s) count: " + parseResult.getWarnings().size());
+        for (int i = 0; i < parseResult.getWarnings().size(); i++) {
+            System.out.println(parseResult.getWarnings().get(i));
+        }
+
+        if (nfa != null) {
+            List<Warning> warnings = nfa.validate();
+            System.out.println("Validation warning(s) count: " + warnings.size());
+            for (Warning warning : warnings){
+                System.out.println(warning);
+            }
+        }
+
     }
 
 }
