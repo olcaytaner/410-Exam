487c7825929143f6fc2b6bcd3cb6934106a21c10
Berra Eğcin <berraegcin@gmail.com>
2025-07-29 00:47:11 +0300

    Update MainPanel.java
    Added: openFile actionListener to open file from computer; pdaButton for PDAPanel

--- src/main/java/UserInterface/MainPanel.java
+++ src/main/java/UserInterface/MainPanel.java
@@ -1,5 +1,8 @@
 package UserInterface;
 
+import javax.swing.filechooser.FileNameExtensionFilter;
+import common.Automaton;
+import PushDownAutomaton.PDA;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -65,26 +68,24 @@ public class MainPanel extends JPanel {
         defaultPages.add(rexButton);
         defaultPages.add(Box.createVerticalStrut(5));
 
-
         JButton tmButton = new JButton("TM");
         defaultPages.add(tmButton);
         defaultPages.add(Box.createVerticalStrut(5));
 
-
-        JLabel openSavedLabel = new JLabel("Open Saved Pages");
-        savedPages.add(openSavedLabel);
+        JButton openFile = new JButton("Open File");
+        openFile.setBackground(Color.lightGray);
+        savedPages.add(openFile);
 
         savedPages.add(Box.createVerticalStrut(10));
 
-        JButton deneme = new JButton("deneme");
-        savedPages.add(deneme);
+        JLabel openSavedLabel = new JLabel("Recent Files");
+        savedPages.add(openSavedLabel);
 
         for(File a : savedPageList ){
             String name = a.getName();
             JButton jbutton = new JButton(name); // farklı buttonlar oluşturuyor mu
             savedPages.add(jbutton);
             savedPages.add(Box.createVerticalStrut(5));
-
         }
 
         this.add(Box.createHorizontalGlue());
@@ -96,12 +97,71 @@ public class MainPanel extends JPanel {
         nfaButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                NFAPanel nfaPanel = new NFAPanel(tabbedPane, MainPanel.this);
+               
+               Automaton nfaMachine = new NFAMachine();
+                NFAPanel nfaPanel = new NFAPanel(tabbedPane, MainPanel.this, nfaMachine);
                 tabbedPane.addTab("NFA", nfaPanel);
                 tabbedPane.setSelectedComponent(nfaPanel);
             }
         });
 
+        pdaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Automaton pdaMachine = new PDA();           
+                PDAPanel pdaPanel = new PDAPanel(tabbedPane, MainPanel.this, pdaMachine);
+                tabbedPane.addTab("PDA", pdaPanel);
+                tabbedPane.setSelectedComponent(pdaPanel);
+            }
+        });
+
+        openFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser fileChooser = new JFileChooser();
+                FileNameExtensionFilter filter = new FileNameExtensionFilter("Automaton Files", "nfa", "pda", "tm", "dfa", "rex", "cfg","txt");
+                fileChooser.setFileFilter(filter);
+                int option = fileChooser.showOpenDialog(MainPanel.this);
+                if (option == JFileChooser.APPROVE_OPTION) {
+                    File file = fileChooser.getSelectedFile();
+                    savedPageList.add(file);
+                    String name = file.getName();
+                    String extension = name.substring(name.lastIndexOf("."));
+                    
+                    try {
+                        String content = new String(java.nio.file.Files.readAllBytes(file.toPath()));
+                        Automaton automaton = null;
+                        JPanel panel = null;
+                        
+                        switch(extension) {
+                            case ".nfa":
+                                automaton = new NFAMachine();
+                                automaton.setInputText(content);
+                                panel = new NFAPanel(tabbedPane, MainPanel.this, automaton);
+                                break;
+                            case ".pda":
+                                automaton = new PDA();
+                                automaton.setInputText(content);
+                                panel = new PDAPanel(tabbedPane, MainPanel.this, automaton);
+                                ((PDAPanel)panel).loadFile(file);
+                                savedPageList.add(file);
+                                tabbedPane.addTab(file.getName(), panel);
+                                tabbedPane.setSelectedComponent(panel);
+                                break;
+                            // Add other cases for different automaton types
+                        }
+                    } catch (Exception ex) {
+                        ex.printStackTrace();
+                        JOptionPane.showMessageDialog(MainPanel.this, 
+                            "Error reading file: " + ex.getMessage(),
+                            "Error",
+                            JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            }
+
+    
+        });
 
 
     }
